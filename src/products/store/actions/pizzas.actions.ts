b67666b99a26a70c2589ad45727a4793c1e20827
Filip Kudla <filipkud31@gmail.com>
2020-09-25 16:11:04 +0200

    craete first reducers and actions for pizzas

---
+++ src/products/store/actions/pizzas.actions.ts
@@ -0,0 +1,27 @@
+import { Action } from '@ngrx/store'
+
+import { Pizza } from '../../models/pizza.model'
+
+// laod pizzas
+export const LOAD_PIZZAS = '[Products] Load pizzas'
+export const LOAD_PIZZAS_FAIL = '[Products] Load pizzas fail'
+export const LOAD_PIZZAS_SUCCESS = '[Products] Load pizzas success'
+
+// creators
+export class LoadPizzas implements Action {
+  readonly type = LOAD_PIZZAS;
+}
+
+export class LoadPizzasFail implements Action {
+  readonly type = LOAD_PIZZAS_FAIL;
+  constructor(public payload: any) {}
+}
+
+export class LoadPizzasSuccess implements Action {
+  readonly type = LOAD_PIZZAS_SUCCESS;
+  constructor(public payload: Pizza[]) {}
+}
+
+
+// action types
+export type PizzasAction = LoadPizzas | LoadPizzasFail | LoadPizzasSuccess
